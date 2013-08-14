05c7db2a20e77d74ccc75626861fe14f740a2eb1
Serqueg10 <sergiogr9506@gmail.com>
2013-08-14 10:38:51 -0500

    cero y van tres que me ponen a hacer lo mismo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miproyecone::Application.config.secret_key_base = '5a15c0d0050d7aecc7e7498a02aaf9956ac195ae0eaa5f8e94ad286b7b3e784c1d2050649458a940e2d45ba882960da91a9c5b43887ffa917d4f3b1ddfe82d18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Miproyecone::Application.config.session_store :cookie_store, key: '_Miproyecone_session'
